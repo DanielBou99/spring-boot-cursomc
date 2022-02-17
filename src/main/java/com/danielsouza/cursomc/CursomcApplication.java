93c163c6ef91642f2995049a967b4d8cfac5d963
Daniel <danielbou@outlook.com.br>
2022-02-17 18:10:51 -0300

    Adicionando entidades Cliente, TipoCliente, Telefone e Endereco

--- src/main/java/com/danielsouza/cursomc/CursomcApplication.java
+++ src/main/java/com/danielsouza/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.danielsouza.cursomc.domain.Categoria;
 import com.danielsouza.cursomc.domain.Cidade;
+import com.danielsouza.cursomc.domain.Cliente;
+import com.danielsouza.cursomc.domain.Endereco;
 import com.danielsouza.cursomc.domain.Estado;
 import com.danielsouza.cursomc.domain.Produto;
+import com.danielsouza.cursomc.domain.enums.TipoCliente;
 import com.danielsouza.cursomc.repositories.CategoriaRepository;
 import com.danielsouza.cursomc.repositories.CidadeRepository;
+import com.danielsouza.cursomc.repositories.ClienteRepository;
+import com.danielsouza.cursomc.repositories.EnderecoRepository;
 import com.danielsouza.cursomc.repositories.EstadoRepository;
 import com.danielsouza.cursomc.repositories.ProdutoRepository;
 
@@ -20,16 +25,22 @@ import com.danielsouza.cursomc.repositories.ProdutoRepository;
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repoCat;
+	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	private ProdutoRepository repoProd;
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
-	private CidadeRepository repoCid;
+	private CidadeRepository cidadeRepository;
 	
 	@Autowired
-	private EstadoRepository repoEst;
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,10 +56,10 @@ public class CursomcApplication implements CommandLineRunner {
 	     Produto p2 = new Produto(null, "Impressora", 800.00);
 	     Produto p3 = new Produto(null, "Mouse", 80.00);
 	 
-	     repoCat.saveAll(Arrays.asList(cat1, cat2));
-	     repoProd.saveAll(Arrays.asList(p1,p2,p3));
-	     repoCat.flush();
-	     repoProd.flush();
+	     categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	     produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	     categoriaRepository.flush();
+	     produtoRepository.flush();
 	 
 	     cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 	     cat2.getProdutos().add(p2);
@@ -56,8 +67,8 @@ public class CursomcApplication implements CommandLineRunner {
 	     p1.getCategorias().add(cat1);
 	     p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 	     p3.getCategorias().add(cat1);
-	     repoCat.saveAll(Arrays.asList(cat1, cat2));
-	     repoProd.saveAll(Arrays.asList(p1,p2,p3));
+	     categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	     produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	     
 	     Estado est1 = new Estado(null, "Minas Gerais");
 	     Estado est2 = new Estado(null, "São Paulo");
@@ -69,8 +80,20 @@ public class CursomcApplication implements CommandLineRunner {
 	     est1.getCidades().addAll(Arrays.asList(c1));
 	     est2.getCidades().addAll(Arrays.asList(c2,c3));
 	     
-	     repoEst.saveAll(Arrays.asList(est1,est2));
-	     repoCid.saveAll(Arrays.asList(c1,c2,c3));
+	     estadoRepository.saveAll(Arrays.asList(est1,est2));
+	     cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+	     
+	     Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "123456789", TipoCliente.PESSOAFISICA);
+	     cli1.getTelefones().addAll(Arrays.asList("2693547852","985432546"));
+	     
+	     Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "2325845", cli1, c1);
+	     Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "1234845", cli1, c2);
+	     
+	     cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+	     
+	     clienteRepository.saveAll(Arrays.asList(cli1));
+	     enderecoRepository.saveAll(Arrays.asList(e1,e2));
+	     
 	}
 
 }

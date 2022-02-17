1f653a3d1512a5c020219f04e73007be64d881a0
Daniel <danielbou@outlook.com.br>
2022-02-17 17:05:35 -0300

    Resolvendo problema ciclico em associação de entidades e ExceptionHandler

--- src/main/java/com/danielsouza/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielsouza/cursomc/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.danielsouza.cursomc.domain.Categoria;
 import com.danielsouza.cursomc.repositories.CategoriaRepository;
+import com.danielsouza.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Nenhuma categoria encontrada para ID: " + id + "."));
 	}
-	
+
 }

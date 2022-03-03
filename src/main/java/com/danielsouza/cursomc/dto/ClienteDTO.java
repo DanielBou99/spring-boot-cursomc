66a0d5887ffd18eb29c345e55c9acea2233cd859
Daniel <danielbou@outlook.com.br>
2022-03-03 13:37:22 -0300

    Validação customizada para não permitir email repetido em Udpate Cliente

--- src/main/java/com/danielsouza/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/danielsouza/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.danielsouza.cursomc.domain.Cliente;
+import com.danielsouza.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
